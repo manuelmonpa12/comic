cc62b7d36e8522cd03659d4f32a518e49eae1080
Manuel Montero <manuel.montero@linktic.co>
2021-10-22 11:48:51 -0500

    cors

--- src/api/comic.js
+++ src/api/comic.js
@@ -5,7 +5,11 @@ const config = {};
 export const getComic = () => {
     
   config.headers = {'Access-Control-Allow-Origin':'*',
-  'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept'}
+  'Access-Control-Allow-Headers':'Origin, X-Requested-With, Content-Type, Accept',
+  'Content-Type': 'application/json',
+}
+config.withCredentials = true
+config.mode= 'no-cors'
   const res = axios.get(`${URL_API}/info.0.json`, config);
 // const res = axios.create({
 //     baseURL: "http://xkcd.com/info.0.json",
